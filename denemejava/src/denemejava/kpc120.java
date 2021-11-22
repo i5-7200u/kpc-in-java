daba124fd05a2802b0e71d7ed5f9c1eb8cf37f6e
i5-7200u <81111892+i5-7200u@users.noreply.github.com>
2021-11-22 16:10:07 +0300

    Add files via upload

---
+++ denemejava/src/denemejava/kpc120.java
@@ -0,0 +1,29 @@
+package denemejava;
+public class qwe {
+    public static void main(String[] args)
+    {
+System.out.println("KCS Password Creator 1.2.0 (Java)");
+java.util.Random qweh = new java.util.Random();
+String hoh = "qwertyuiopasdfghjkl;'zxcvbnm,./1234567890";
+java.util.Scanner ehj = new java.util.Scanner(System.in);
+System.out.println("Write the to be generated length of random chars:");
+int ewqd = ehj.nextInt();
+if (ewqd == 0)
+{
+    ewqd = 50;
+}
+System.out.println("Write the to be generated characters of random chars, If you write nothing then generated random chars with default chars:");
+ehj.nextLine();
+String charsw = ehj.nextLine();
+if (charsw.replace(" ","") != "")
+{
+    hoh = charsw.replace(" ","");
+}
+String hauh = "";
+for(int h = 0; h < ewqd; h++)
+{
+hauh = hauh + hoh.charAt(qweh.nextInt(hoh.length()));
+}
+System.out.println(hauh);
+}
+}
